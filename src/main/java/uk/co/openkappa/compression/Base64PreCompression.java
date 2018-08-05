f4cc3aebacd905ad45ed2d407387eef0a3e4f3a2
Richard Startin <richard@openkappa.co.uk>
2018-08-05 21:37:38 +0100

    cleanup

--- src/main/java/uk/co/openkappa/compression/Base64PreCompression.java
+++ src/main/java/uk/co/openkappa/compression/Base64PreCompression.java
@@ -4,21 +4,24 @@ import java.util.function.Function;
 
 import static java.util.Arrays.asList;
 import static uk.co.openkappa.compression.CompressionBenchmark.benchmark;
+import static uk.co.openkappa.compression.Util.base64;
+import static uk.co.openkappa.compression.Util.lz4;
+import static uk.co.openkappa.compression.Util.snappy;
 
 public class Base64PreCompression {
 
   public static void main(String[] args) {
     var encodings = asList(
             Encoding.of("binary", Function.identity()),
-            Encoding.of("base64", Util.base64),
-            Encoding.of("base64/snappy", Util.base64.andThen(Util.snappy)),
-            Encoding.of("base64/lz4", Util.base64.andThen(Util.lz4))
+            Encoding.of("base64", base64),
+            Encoding.of("base64/snappy", base64.andThen(snappy)),
+            Encoding.of("base64/lz4", base64.andThen(lz4))
     );
     var compressions = asList(
             new Uncompressed<byte[]>(),
             new Gzip<byte[]>()
     );
-    //benchmark(encodings, compressions, Util::monotonicKiloBytes, 1_000_000);
+    benchmark(encodings, compressions, Util::monotonicKiloBytes, 1_000_000);
     benchmark(encodings, compressions, Util::sinusoidalKiloBytes, 1_000_000);
   }
 }
